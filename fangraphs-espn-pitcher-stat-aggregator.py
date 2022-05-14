880529ee14e1a58eb4d5a2d50991766108b814b7
Nathanael Mathieu <nmathieu@wesleyan.edu>
2022-05-14 00:53:03 -0400

    old changes that are definitely good

--- fangraphs-espn-pitcher-stat-aggregator.py
+++ fangraphs-espn-pitcher-stat-aggregator.py
@@ -15,11 +15,12 @@ py -3.7 fangraphs-espn-pitcher-stat-aggregator.py -a="['xFIP', 'ERA', 'K/9', 'St
 '''
 from espn_api.baseball import League
 from baseball_scraper import pitching_stats
+from datetime import date
 import argparse
 import ast
 import csv
 
-def printStats(player_name, player_type, args, fg=None):
+def printLine(player_name, player_type, args, doPrint, fg=None):
     if fg is not None:
         fg_player = fg.query('Name=="{}"'.format(player_name))
         stats = []
@@ -28,15 +29,16 @@ def printStats(player_name, player_type, args, fg=None):
             except: stats.append(0)
         args = stats
 
-    column = "%10s | %20s |" % (player_type, player_name)
-    for val in args:
-        column = column + " %7s |" % (str(val))
-    print(column)
+    if(doPrint):
+        column = "%10s | %20s |" % (player_type, player_name)
+        for val in args:
+            column = column + " %7s |" % (str(val))
+        print(column)
 
-    if fg is None:
-        print('-' * len(column))
+        if fg is None:
+            print('-' * len(column))
 
-    with open('out/fangraphs-espn-pitcher-stats.csv', 'a', newline='') as csvfile:
+    with open('out/fangraphs-espn-pitcher-stats-{}.csv'.format(date.today()), 'a', newline='') as csvfile:
         writer = csv.writer(csvfile)
         new_line = [player_type, player_name, *args]
         writer.writerow(new_line)
@@ -48,6 +50,7 @@ def main():
     parser.add_argument('-l', '--league-id', type=int, default=45839, help='ESPN league id. Must be public. Default=45839')
     parser.add_argument('-t', '--team-id', type=int, default=4, help='ESPN team ID. Default=4')
     parser.add_argument('-n', '--number-of-fa', type=int, default=100, help='Number free agents on ESPN to search. Default=100')
+    parser.add_argument('-p', '--print', type=bool, default=False, help='Print the stats to the terminal. Default=False')
     args = parser.parse_args()
     
     arguments = args.args
@@ -55,23 +58,26 @@ def main():
     league_id = args.league_id
     team_id = args.team_id
     size = args.number_of_fa
+    doPrint = args.print
 
+    print('Fetching ESPN Data...')
     league = League(league_id=league_id, year=current_season)
-
     my_roster = league.get_team_data(team_id).roster
     free_agent_pitchers = league.free_agents(size=size, position='P')
 
+    print('Fetching Fangraphs Data...')
     fangraphs = pitching_stats(current_season, current_season)
 
-    printStats("Player Name", "Roster", arguments)
-
+    print('Aggregating stats...')
+    printLine("Player Name", "Roster", arguments, doPrint)
     for fantasy_player in my_roster:
         if 'P' in fantasy_player.eligibleSlots:
-            printStats(fantasy_player.name, "My Team", arguments, fangraphs)
+            printLine(fantasy_player.name, "My Team", arguments, doPrint, fangraphs)
 
     for fantasy_player in free_agent_pitchers:
         if 'P' in fantasy_player.eligibleSlots:
-            printStats(fantasy_player.name, "Free Agent", arguments, fangraphs)
+            printLine(fantasy_player.name, "Free Agent", arguments, doPrint, fangraphs)
+    print('Done.')
 
 if __name__ == "__main__":
    main()
